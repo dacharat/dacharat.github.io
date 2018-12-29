7a0fe759a8435d1638982bb77cd205a873ae38e0
dacharat <dacharat2013@gmail.com>
2018-12-29 23:23:17 +0700

    Updates

---
+++ precache-manifest.6c1dfb7109dcbfd6a42d14549c614b5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "516e5054688c0a4823da",
+    "url": "/static/css/main.d3266510.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f72bdd6c2a72b51448a1",
+    "url": "/static/js/1.f72bdd6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "904c03e9a23a2846c281f00d8579796f",
+    "url": "/static/media/profile.904c03e9.png"
+  },
+  {
+    "revision": "516e5054688c0a4823da",
+    "url": "/static/js/main.516e5054.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "f72bdd6c2a72b51448a1",
+    "url": "/static/css/1.01b7965d.chunk.css"
+  },
+  {
+    "revision": "698b1f16d10a9a8443da8f6ac041b420",
+    "url": "/index.html"
+  }
+];
