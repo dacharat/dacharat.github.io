166a33675c2486e6d5592581a36cbc0040b69ab8
dacharat <dacharat2013@gmail.com>
2018-12-30 12:22:47 +0700

    :octocat: add skill

--- src/components/about/skill.js
+++ src/components/about/skill.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Segment from '../segment'
+
+const Body = (<div><h1>React</h1></div>)
 
 const Skill = () => {
-  return <div>Skill</div>;
+  return <Segment icon="align-left" name="Skill" component={Body} />;
 };
 
 export default Skill;
--- src/segments/about.js
+++ src/segments/about.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutMe from "../components/about/about_me";
 import Information from "../components/about/information";
+import Skill from '../components/about/skill'
 
 const About = () => {
   return (
     <div className="container">
       <AboutMe />
       <Information />
+      <Skill />
     </div>
   );
 };
