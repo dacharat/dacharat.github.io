4dc734d89de0c95ed84a2ffcf00b6ae1fb003b59
dacharat <dacharat.p@ku.th>
2020-01-26 23:17:41 +0700

    :octocat: add wongnai internship

--- src/components/experience/experience_list.js
+++ src/components/experience/experience_list.js
@@ -6,6 +6,7 @@ import { Flip } from "react-reveal";
 // experiences
 import glazziq from "../../assets/experiences/glazziq.jpg";
 import offenburg from "../../assets/experiences/offenburg_university.jpg";
+import wongnai from "../../assets/experiences/wongnai.jpg";
 
 const myExperience = [
   {
@@ -30,6 +31,20 @@ const myExperience = [
       "OpenCV, Tensorflow, Keras",
       "Django Framework",
       "PostgreSQL"
+    ]
+  },
+  {
+    name: "Internship at Wongnai Media Co.,Ltd.",
+    image: wongnai,
+    describe: [
+      "Software Engineer, Backend [Growth squad]",
+      "13/08/2019 - 20/12/2019",
+      "Java, Python, Javascript, Typescript",
+      "RESTful API, gRPC, graphQL",
+      "React",
+      "Express",
+      "Spring Framework",
+      "MySQL, phpMyAdmin"
     ]
   }
 ];
