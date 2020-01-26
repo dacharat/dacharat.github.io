85e04df29dd2622ef04b930b56cdeb704758663f
dacharat <dacharat.p@ku.th>
2020-01-26 23:21:56 +0700

    Updates

---
+++ precache-manifest.883cd3dd275e866cf4c9305c7262b03b.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e502e6b8e49531f3e9bdb61deeb5d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52cef8371338511cb6aa",
+    "url": "/static/css/2.5b14e42c.chunk.css"
+  },
+  {
+    "revision": "52cef8371338511cb6aa",
+    "url": "/static/js/2.fb504096.chunk.js"
+  },
+  {
+    "revision": "8244bce7175e0ac5d316",
+    "url": "/static/js/main.9706c29a.chunk.js"
+  },
+  {
+    "revision": "0c197d17ae9a2f88acde",
+    "url": "/static/js/runtime~main.15860891.js"
+  },
+  {
+    "revision": "e8d3f48831d28caf418529facf42bd2e",
+    "url": "/static/media/android.e8d3f488.png"
+  },
+  {
+    "revision": "834f9eab78e7377e87a0b8680b3dcfc4",
+    "url": "/static/media/asana.834f9eab.png"
+  },
+  {
+    "revision": "562eee740b5e72b1f51605745326360e",
+    "url": "/static/media/c.562eee74.png"
+  },
+  {
+    "revision": "3d87366333232f8748d5de519eeaa681",
+    "url": "/static/media/cover-mobile.3d873663.jpg"
+  },
+  {
+    "revision": "035ca369a2fd91aba3dc967a48e6d558",
+    "url": "/static/media/cover-pc.035ca369.jpg"
+  },
+  {
+    "revision": "bb4c6b29ee6bae5a8048be559aefbbae",
+    "url": "/static/media/cover-tablet.bb4c6b29.jpg"
+  },
+  {
+    "revision": "02711c2cf037c786ebb844af1328f809",
+    "url": "/static/media/django.02711c2c.png"
+  },
+  {
+    "revision": "b1ef2fe8f9edf2b0d08fd9fbf2aeb619",
+    "url": "/static/media/fifth.b1ef2fe8.png"
+  },
+  {
+    "revision": "a0460b95a432081ebb4979800f2cc53e",
+    "url": "/static/media/firebase.a0460b95.png"
+  },
+  {
+    "revision": "04fa1f6deeb4272397f6932b6f357274",
+    "url": "/static/media/first.04fa1f6d.png"
+  },
+  {
+    "revision": "06cc33369bf2404e81f35a8189e06772",
+    "url": "/static/media/first.06cc3336.png"
+  },
+  {
+    "revision": "840093ca8efb91e9d6abdffff05ff222",
+    "url": "/static/media/first.840093ca.png"
+  },
+  {
+    "revision": "aec36132dc82638e9c33121d64948817",
+    "url": "/static/media/first.aec36132.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eb3b00436ed201256894e84689ceb836",
+    "url": "/static/media/fourth.eb3b0043.png"
+  },
+  {
+    "revision": "fb543162226394388e25a7595d251689",
+    "url": "/static/media/fourth.fb543162.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/static/media/github.3e54ed15.png"
+  },
+  {
+    "revision": "11d38b71ab182e64a9440177f00030d4",
+    "url": "/static/media/gitlab.11d38b71.png"
+  },
+  {
+    "revision": "58e3d8766730d4a958d54fb6f072ffa7",
+    "url": "/static/media/glazziq.58e3d876.jpg"
+  },
+  {
+    "revision": "2c73e19b097d584f13d8d652a4a77d59",
+    "url": "/static/media/java.2c73e19b.png"
+  },
+  {
+    "revision": "3f27dbb7bfd8b2b440ba45c1aa30d5d4",
+    "url": "/static/media/javafx.3f27dbb7.png"
+  },
+  {
+    "revision": "aac4650a77ca30f2fda52d0ab1d8b609",
+    "url": "/static/media/javascript.aac4650a.png"
+  },
+  {
+    "revision": "4bda24982455aa53c4c6362239e1e3bc",
+    "url": "/static/media/keras.4bda2498.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/static/media/mongodb.e5eee315.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/static/media/node.6dcf6999.png"
+  },
+  {
+    "revision": "0ce0251b5226f038ebdfa884759f1b09",
+    "url": "/static/media/nvidia.0ce0251b.png"
+  },
+  {
+    "revision": "f69f343591c2e21a1023baf4f723958c",
+    "url": "/static/media/offenburg_university.f69f3435.jpg"
+  },
+  {
+    "revision": "591dbc0fb4f609461beaae0951c0362b",
+    "url": "/static/media/opencv.591dbc0f.png"
+  },
+  {
+    "revision": "ea5a6b4e047b0d965fbef3699466da8d",
+    "url": "/static/media/postgreSQL.ea5a6b4e.png"
+  },
+  {
+    "revision": "904c03e9a23a2846c281f00d8579796f",
+    "url": "/static/media/profile.904c03e9.png"
+  },
+  {
+    "revision": "79112b07e350620c05e7679450afe491",
+    "url": "/static/media/rails.79112b07.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react-native.47ce6e77.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "0ea7fa18e05c523467e8650a3e94b89a",
+    "url": "/static/media/ruby.0ea7fa18.png"
+  },
+  {
+    "revision": "0de745de5156d42cccf3f4fde5ed084a",
+    "url": "/static/media/second.0de745de.png"
+  },
+  {
+    "revision": "567509dd210e3a9b3db86c77b91ea711",
+    "url": "/static/media/second.567509dd.png"
+  },
+  {
+    "revision": "93707e315807e3832ef13d061d8a1023",
+    "url": "/static/media/second.93707e31.png"
+  },
+  {
+    "revision": "ff550d11e2ed6de5c9dcc7023a3a7455",
+    "url": "/static/media/second.ff550d11.png"
+  },
+  {
+    "revision": "7329e21a49425be4830725a5f121e664",
+    "url": "/static/media/sixth.7329e21a.png"
+  },
+  {
+    "revision": "097b189d0b38518e83d19733ae71a5cf",
+    "url": "/static/media/slack.097b189d.png"
+  },
+  {
+    "revision": "a1c911c4bba7c110d023cf5e8a236550",
+    "url": "/static/media/tensorflow.a1c911c4.png"
+  },
+  {
+    "revision": "2b416ea18d3544e8e11c1ef8402b8202",
+    "url": "/static/media/third.2b416ea1.png"
+  },
+  {
+    "revision": "659c9976fe48e8c78472f08c50cdeca3",
+    "url": "/static/media/third.659c9976.png"
+  },
+  {
+    "revision": "5b7c7c68c27f56eb1b616d0dcece179b",
+    "url": "/static/media/wongnai.5b7c7c68.jpg"
+  },
+  {
+    "revision": "4725ad9f0529247dc705536a2a8ef543",
+    "url": "/static/media/xcode.4725ad9f.jpg"
+  }
+]);
